fc210ce832a2c099abdb7b121830859b1d6b37b3
esaphogus <esaphogus@gmail.com>
2023-07-10 23:12:20 +0200

    add landing page, fix text bug

--- tourapp/views.py
+++ tourapp/views.py
@@ -12,17 +12,25 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from rest_framework.views import APIView
 from rest_framework.response import Response
-data = None
+
+
+api_url = f"http://127.0.0.1:8080/best-tours/50"
+response_data = requests.get(api_url)
+response_content = json.loads(response_data.content)
+data = {'tours': response_content['best_tours']}
+
 @csrf_exempt
 def home(request):
-    return render(request, 'tourapp/home.html')
+    return render(request, 'tourapp/home.html', context=data)
 
-
+@csrf_exempt
+def home_details(request,card_id):
+    print(card_id)
+    tours = data['tours']
+    return render(request, 'tourapp/details.html', tours[card_id])
 
 @csrf_exempt
 def details(request, card_id):
-    print(card_id)
-    # print(request.body.decode('utf-8'))
     tour_data = request.body.decode('utf-8')
     tour_data = json.loads(tour_data)
     want_data = tour_data[card_id]
