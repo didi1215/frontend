d9b22fe99abcf02375160146949661983b7c89af
esaphogus <esaphogus@gmail.com>
2023-07-14 14:52:00 +0200

    fix text overflow, change map to scatterplot

--- tourapp/static/tourapp/js/map.js
+++ tourapp/static/tourapp/js/map.js
@@ -1,54 +1,46 @@
-// 创建地图
-function createMap(tourLat, tourLon) {
+function createMap(tourLat, tourLon) {    
     // console.log('------------------------')
-    if (navigator.geolocation) {
+    if (navigator.geolocation) {        
         navigator.geolocation.getCurrentPosition(function (position) {
-            var latitude = position.coords.latitude;
+            var latitude = position.coords.latitude;            
             var longitude = position.coords.longitude;
-
-            // 获取HTML模板中的经纬度值
-            // var tourStartLat = parseFloat("{{ tour_starting_point_lat }}");
-            // var tourStartLon = parseFloat("{{ tour_starting_point_lon }}");
-
-            var lineData = [{
-                type: 'scattermapbox',
-                mode: 'lines',
-                lon: [longitude, tourLon, null],
-                lat: [latitude, tourLat, null],
-                marker: {
-                    size: 14
-                },
-                text: ['Current Location', 'Tour Starting Point']
-            }];
-            // console.log(lineData+'.....')
-            // var data = [
-                // lineData,
-                // {
-                //     type: 'scattermapbox',
-                //     lat: [tourLat],
-                //     lon: [tourLon],
-                //     mode: 'markers',
-                //     marker: {
-                //         size: 14
-                //     },
-                //     text: ['Starting Point']
-                // }
-            // ];
-
-            var layout = {
-                autosize: true,
-                hovermode: 'closest',
-                mapbox: {
-                    center: {
-                        lat: parseFloat(tourLat),
-                        lon: parseFloat(tourLon)
+                        
+            var data = [
+                {                    
+                    type: 'scattermapbox',
+                    lat: [tourLat],                    
+                    lon: [tourLon],
+                    mode: 'markers',                    
+                    marker: {
+                        size: 14                    
                     },
+                    text: ['Starting Point'],
+                    name: 'Starting Point',               
+                },
+                {                    
+                    type: 'scattermapbox',
+                    lat: [latitude],                    
+                    lon: [longitude],
+                    mode: 'markers',                    
+                    marker: {
+                        size: 14                    
+                    },
+                    text: ['Current Location'],
+                    name: 'Current Location'                
+                }
+            ];
+            var layout = {
+                autosize: true,                
+                hovermode: 'closest',
+                mapbox: {                    
+                    center: {
+                        lat: parseFloat(tourLat),                        
+                        lon: parseFloat(tourLon)
+                    },                   
                     zoom: 10,
-                    style: 'open-street-map'
+                    style: 'open-street-map'                
                 }
             };
-
-            Plotly.newPlot('mymap', lineData, layout);
+            Plotly.newPlot('mymap', data, layout);        
         });
-    }
-}
+    }}
